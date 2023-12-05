ce6052efdf3c3b96f0d4508d2fd0fc9ffda50ed9
Varisck <albyvaro@gmail.com>
2023-12-05 16:54:31 +0100

    test for automata

--- unialgo/pattern/match_test.cc
+++ unialgo/pattern/match_test.cc
@@ -1,9 +1,42 @@
 #include <gtest/gtest.h>
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+#include <string>
+#include <vector>
+
+#include "unialgo/pattern/stringMatching.hpp"
+
+namespace {
+
+TEST(FinateStateAutomataTest, BasicTests) {
+  // Test case 1: Pattern occurs multiple times in the text
+  std::string text1 = "abababcabab";
+  std::string pattern1 = "ab";
+  std::vector<size_t> result1 = unialgo::pattern::Fsa(text1, pattern1);
+  std::vector<size_t> expected1 = {0, 2, 4, 7, 9};
+  EXPECT_EQ(result1, expected1);
+
+  // Test case 2: Pattern occurs once in the text
+  std::string text2 = "Hello, world!";
+  std::string pattern2 = "world";
+  std::vector<size_t> result2 = unialgo::pattern::Fsa(text2, pattern2);
+  std::vector<size_t> expected2 = {7};
+  EXPECT_EQ(result2, expected2);
+
+  // Test case 3: Pattern does not occur in the text
+  std::string text3 = "abcdef";
+  std::string pattern3 = "xyz";
+  std::vector<size_t> result3 = unialgo::pattern::Fsa(text3, pattern3);
+  std::vector<size_t> expected3 = {};
+  EXPECT_EQ(result3, expected3);
 }
+
+TEST(FinateStateAutomataTest, PatternLongerThenText) {
+  // Test case 1: Pattern longer then text
+  std::string text = "a";
+  std::string pattern = "xyz";
+  std::vector<size_t> result = unialgo::pattern::Fsa(text, pattern);
+  std::vector<size_t> expected = {};
+  EXPECT_EQ(result, expected);
+}
+
+}  // namespace
